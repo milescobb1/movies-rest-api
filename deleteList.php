048e6350390f07dd418d969e6249e8d77298a57e
milescobb1 <miles.cobb@gmail.com>
2018-02-19 23:50:47 -0800

    final updates

--- deleteList.php
+++ deleteList.php
@@ -4,7 +4,6 @@ try{
    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $query = "DELETE FROM lists WHERE name=? AND UID = ?;";
       $params = array($_POST['name'], $_POST['UID']);
-      print_r($params);
       $statement = $db -> prepare($query);
       $statement->execute($params);
    }
